4c43a7ab2170433e626e272eb96128ebf0578b9d
Gabriel Romao <gabriel-rgomes@hotmail.com>
2023-09-28 11:46:32 -0300

    improve annotation putting in another ax

--- main.py
+++ main.py
@@ -9,24 +9,31 @@ variance = 10
 
 X, y = lr.generate_linear_data(x_range, slope, intercept, variance)
 
+fig, (ax_main, ax_annotations) = plt.subplots(2, 1, gridspec_kw={'height_ratios': [6, 1]})
 
 for index in range(1, len(X) + 1):
-    
+    ax_main.clear()
+    ax_annotations.clear()
+
     partial_y = y[:index]
     partial_X = X[:index]
-    
-    m, b = lr.get_slope_intercept(partial_X, partial_y)
-    y_line = [m*x + b for x in X]
-    mae = lr.mean_absolute_error(y, y_line)
-    
-    plt.xlim((x_range[0] - 30, x_range[1] + 5))
-    plt.ylim((np.min(y) -40, np.max(y) + 10))
 
-    plt.scatter(X[index:], y[index:])
-    plt.scatter(X[:index], partial_y)
-    plt.plot(X, y_line, color='orange')
-    plt.annotate(f"mae = {mae:.2f} \nModel: y = {m:.2f}*x + {b:.2f} \nReal: y = {slope}*x + {intercept}", (x_range[0] - 27, np.min(y) - 35))
-    
-    input('Pressione Enter para continuar')
-    
-    plt.show()
+    m, b = lr.get_slope_intercept(partial_X, partial_y)
+    y_line = [m * x + b for x in X]
+    mae = lr.mean_absolute_error(y, y_line)
+
+    ax_main.set_xlim((x_range[0] - 10, x_range[1] + 10))
+    ax_main.set_ylim((np.min(y) - 10, np.max(y) + 10))
+
+    ax_main.scatter(X[index:], y[index:])
+    ax_main.scatter(X[:index], partial_y)
+    ax_main.plot(X, y_line, color='orange')
+
+    ax_annotations.text(0, 0,
+                        f"mae = {mae:.2f} \nModel: y = {m:.2f}*x + {b:.2f} \nReal: y = {slope}*x + {intercept}",
+                        fontsize=12, color='black')
+
+    ax_annotations.axis('off')
+    plt.subplots_adjust(hspace=0.25)
+
+    plt.pause(0.5)
